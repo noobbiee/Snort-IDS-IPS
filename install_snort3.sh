d0a8c8cc38f064de67cd20ff636dfde09ae906b0
noobbiee <155554667+noobbiee@users.noreply.github.com>
2024-10-15 00:20:44 +1100

    Update install_snort3.sh

--- install_snort3.sh
+++ install_snort3.sh
@@ -95,4 +95,44 @@ touch /etc/snort/snort_defaults.lua
 touch /etc/snort/file_magic.lua
 
 
+-- create a new snort systemd file to put the interface in promisc mode when snort starts
+
+touch /etc/systemd/system/snort3-nic.service
+
+cat <<EOF > /etc/systemd/system/snort3-nic.service
+[Unit]
+Description=Set Snort 3 NIC in promiscuous mode and Disable GRO, LRO on boot
+After=network.target
+
+[Service]
+Type=oneshot
+ExecStart=/usr/sbin/ip link set dev eth0 promisc on
+ExecStart=/usr/sbin/ethtool -K eth0 gro off lro off
+TimeoutStartSec=0
+RemainAfterExit=Yes
+
+[Install]
+WantedBy=default.target
+EOF
+
+
+ip link set dev eth0 promisc on
+if ethtool -k eth0 | grep -q 'generic-receive-offload: off'; then
+	echo "generic-receive-offload is off"
+else
+	ethtool -K eth0 gro off
+fi
+
+systemctl daemon-reload
+systemctl start snort3-nic.service
+systemctl enable snort3-nic.service
+
+# create a new directory to store the rules file
+mkdir /usr/local/etc/rules
+cd /usr/local/etc/rules
+wget -qo- https://www.snort.org/downloads/community/snort3-community-rules.tar.gz
+tar -xzf snort3-community-rules.tar.gz
+
+echo "open the configuration file and add the Home network and include the rulesfiles path to assign rules to your snort machine"
+
 
